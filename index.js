bfd376aee4c90ca9ae70c9429f814d7c0f9bc0ab
yeray <yeray.programacion.web@gmail.com>
2023-09-02 00:48:25 +0100

    ventana start css y evento click reiniciar en proceso, falta borrar los personajes y mirar el bug de la colision que no se para.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var plant = new Plants(plantX, plantY, board)
 plant.spawnPlant()
 player.spawnZombie()
 
-function checkCollition(){
+function checkCollition() {
     if (
         plant.y + plant.height >= player.y &&
         plant.y <= player.y + player.height &&
@@ -24,26 +24,31 @@ function checkCollition(){
         player.dead = true
     }
 
-    
+
 }
 
-function zombieMovement(){
-        meta()
-        checkCollition()
-        if(player.dead){
-            alert('Game Over')
-            clearInterval(timerId)
-        }
+function zombieMovement() {
+    meta()
+    checkCollition()
+    if (player.dead) {
+        //alert('Game Over')
+        clearInterval(timerId)
+        //IMPLEMENTAR SI ESTA MUERTO ----(FALTA ELIMINAR LA PLANTA Y EL ZOMBIE)
+
+        //inicia el juego
+        iniciarJuego()
+    }
     player.move()
 }
+
 // Bucles
 var timerId = setInterval(zombieMovement, 30)
-
 
 var meta = function () {
     if (player.y <= 30) {
         clearInterval(timerId)
-        alert("Victoria")
+        //alert("Victoria")
+
     }
 }
 
@@ -64,3 +69,31 @@ window.addEventListener('keyup', function () {
     player.direction = 0
 })
 
+/*Evento del Start y Game Over*/
+var startGame = document.getElementById('start-game');
+startGame.addEventListener('click', reiniciarTodo)
+
+function reiniciarTodo() {
+    // Reiniciar el estado del juego
+    player.dead = false;
+    // Respawn de la planta y el Zombie
+    plantX = Math.floor(Math.random() * 420)
+    plantY = Math.floor(Math.random() * (600 - 40) + 40)
+    player = new Zombie(200, 770, board)
+    plant = new Plants(plantX, plantY, board)
+
+    plant.spawnPlant();
+    player.spawnZombie();
+
+    // Reanudar el juego
+    //clearInterval(timerId);
+    timerId = setInterval(zombieMovement, 30)
+    //player.move();
+
+}
+
+function iniciarJuego() {
+    if (player.dead === true || player.meta) {//si player.meta no  vale: poner player.y <= 30 --> es lo mismo que meta
+        reiniciarTodo();
+    }
+}
